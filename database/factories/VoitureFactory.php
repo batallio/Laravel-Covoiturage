aae49ca54fdf68fe3b962226e521b42171bfd342
RAMIREZ BATALLA Angel David <contact@angelbatalla.com>
2026-03-12 09:12:36 +0100

    Factories creation

---
+++ database/factories/VoitureFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Voiture;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Voiture>
+ */
+class VoitureFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
